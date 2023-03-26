3087a38ee0152538cbc5c3e4ffb8437ebd5bfc01
yuvraajchouhan <yuvraajchouhan.79@gmail.com>
2023-03-26 13:29:55 -0700

    YC_42_refactored coin class to implement collided interface, and extend sprite. Also changed Player and Edible interface to support polymorphism and inheritance. Thank you Noor for the help.

--- src/main/java/org/bcit/comp2522/project/Collided.java
+++ src/main/java/org/bcit/comp2522/project/Collided.java
@@ -3,6 +3,7 @@ package org.bcit.comp2522.project;
 import processing.core.PVector;
 
 public interface Collided {
+
   public static boolean collided(Sprite a, Sprite b) {
     float distance = PVector.dist(a.getPosition(), b.getPosition());
     if (distance <= (a.getSize() + b.getSize())) {
@@ -10,5 +11,6 @@ public interface Collided {
     }
     return false;
   }
+
 }
 //collided interface
--- src/main/java/org/bcit/comp2522/project/Edible.java
+++ src/main/java/org/bcit/comp2522/project/Edible.java
@@ -2,7 +2,5 @@ package org.bcit.comp2522.project;
 
 public interface Edible {
 
-  public void eaten();
-
 
 }
