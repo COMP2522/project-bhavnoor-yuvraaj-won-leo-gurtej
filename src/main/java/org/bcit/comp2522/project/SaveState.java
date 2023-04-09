126250770833b770062b7229784583e0cd40f08f
Gurtej Malik <gurtejmalik@gmail.com>
2023-04-09 12:00:33 -0700

    GM_107_Made SaveState to write to a json file

--- src/main/java/org/bcit/comp2522/project/SaveState.java
+++ src/main/java/org/bcit/comp2522/project/SaveState.java
@@ -1,5 +1,11 @@
 package org.bcit.comp2522.project;
 
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * The SaveState class represents the data saved for a player in a game.
  * It contains information about the player's health and score.
@@ -15,6 +21,11 @@ public class SaveState {
    * The score of the player.
    */
   private float score;
+
+  /**
+   * The name of the player.
+   */
+  private String name;
 
   /**
    * Constructs a SaveState object.
@@ -49,6 +60,32 @@ public class SaveState {
    */
   public float loadPlayerScore() {
     return score;
+  }
+
+  public void createJSON(String name, float health, float score) throws IOException {
+    JSONObject playerStats = new JSONObject();
+    playerStats.put("Name", name);
+    playerStats.put("Score", score);
+    playerStats.put("Health", health);
+    System.out.println("JSON object created: " + playerStats);
+
+    try {
+      // write to JSON file
+      File jsonFile = new File("player-stats.json");
+      FileWriter fileWriter = new FileWriter(jsonFile, true);
+
+      fileWriter.write(playerStats.toJSONString());
+      fileWriter.flush();
+      fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    SaveState saveState = new SaveState();
+    saveState.createJSON("Pauline", 0, 1000);
+
   }
 
 
